efb5f25aab2f052477440370993b158e2acbbab6
Boris Quizhpe <85261574+Boxvi@users.noreply.github.com>
2025-03-21 17:06:19 +0000

    pureba22

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <p>Un gusto poder conocerte</p>
         <br />
 
-        <a href='public/cv_boris_quizhpe_romero.pdf' download="cv_boris_quizhpe_romero.pdf">
+        <a href="/cv_boris_quizhpe_romero.pdf" download>
           <button className="text-text border-border dark:border-darkBorder shadow-light dark:shadow-dark mx-auto flex w-max gap-5 rounded-base border-2 bg-main p-2.5 px-5 text-sm font-base sm:text-base w450:gap-4">
             Descargar CV</button>
         </a>
